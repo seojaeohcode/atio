78aa08335ca0b58c7c9fc1b09b5c4b25814d8a16
realstone3639 <realstone3639@gmail.com>
2025-08-13 21:18:35 +0900

    register_writer 제외

--- src/atio/__init__.py
+++ src/atio/__init__.py
@@ -7,6 +7,6 @@ __version__ = "1.0.0"
 from .core import write, write_snapshot, read_table, expire_snapshots
 # Public API로 노출할 함수들을 명시적으로 가져옵니다.
 from .core import write
-from .plugins import register_writer
+
 
 # 향후 open, atomic_output 등도 여기에 추가 예정
