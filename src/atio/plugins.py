1558a41b868e9e2c7e61de0f840b54747e3e938f
realstone3639 <realstone3639@gmail.com>
2025-10-14 23:05:41 +0900

    라이선스 헤더 추가

--- src/atio/plugins.py
+++ src/atio/plugins.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Atio Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .utils import setup_logger
 # 추후 Pandas, Polars, Numpy 등 확장 지원을 위한 플러그인 구조
 
--- tests/test_actual_user_logs.py
+++ tests/test_actual_user_logs.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+
+# Copyright 2025 Atio Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 실제 사용자가 보는 로그 확인 테스트
 라이브러리를 사용할 때 실제로 어떤 로그가 출력되는지 확인
